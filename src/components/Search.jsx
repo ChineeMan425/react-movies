f1e5d3a038b313e2ba75db554ce3621caf0ed4b5
Chineeman425 <101083805+ChineeMan425@users.noreply.github.com>
2026-02-02 16:03:53 -0400

    react-movies dashboard
    react-movies dashboard

---
+++ src/components/Search.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Search = ({searchTerm, setSearchTerm}) => {
+    return (
+        <div className={"search"}>
+            <div>
+            <img src={ "src/assets/search.svg"}/>
+
+            <input
+                type={"text"}
+                placeholder={"Search Your Favourite Movie"}
+                value={searchTerm}
+                onChange={(event) => setSearchTerm(event.target.value)}
+            />
+            </div>
+        </div>
+    );
+};
+
+export default Search;
